4b5313d185061ded3e80ba214f8954d070ebeb42
Trine Rye <trine.rye@hotmail.com>
2024-03-07 16:42:56 +0100

    updates to A2

--- assignment_2/logistic_regression.py
+++ assignment_2/logistic_regression.py
@@ -3,6 +3,8 @@
 import os
 import pandas as pd
 
+import utils.classifier_utils as clf
+
 # Machine learning stuff
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -13,5 +15,59 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 
 # %%
+filepath = os.path.join("data","fake_or_real_news.csv")
 
+data = pd.read_csv(filepath, index_col=0)
 
+# %%
+
+data["label"].value_counts()
+
+# %%
+
+X = data["text"]
+y = data["label"]
+# %%
+
+X_train, X_test, y_train, y_test = train_test_split(X,           # texts for the model
+                                                    y,          # classification labels
+                                                    test_size=0.2,   # create an 80/20 split
+                                                    random_state=42) # random state for reproducibility
+# %%
+vectorizer = TfidfVectorizer(ngram_range = (1,2),     # unigrams and bigrams (1 word and 2 word units)
+                             lowercase =  True,       # why use lowercase?
+                             max_df = 0.95,           # remove very common words
+                             min_df = 0.05,           # remove very rare words
+                             max_features = 100)      # keep only top 100 features
+# %%
+# first we fit to the training data...
+X_train_feats = vectorizer.fit_transform(X_train)
+
+#... then do it for our test data
+X_test_feats = vectorizer.transform(X_test)
+
+# get feature names
+feature_names = vectorizer.get_feature_names_out()
+# %%
+classifier = LogisticRegression(random_state=42).fit(X_train_feats, y_train)
+
+y_pred = classifier.predict(X_test_feats)
+
+# %%
+metrics.ConfusionMatrixDisplay.from_estimator(classifier,           # the classifier name
+                                            X_train_feats,          # the training features
+                                            y_train,                # the training labels
+                                            cmap=plt.cm.Blues,      # make the colours prettier
+                                            labels=["FAKE", "REAL"])# the labels in your data arranged alphabetically
+# %%
+classifier_metrics = metrics.classification_report(y_test, y_pred)
+print(classifier_metrics)
+# %%
+X_vect = vectorizer.fit_transform(X)
+
+title = "Learning Curves (Logistic Regression)"
+cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+
+estimator = LogisticRegression(random_state=42)
+clf.plot_learning_curve(estimator, title, X_vect, y, cv=cv, n_jobs=4)
+# %%
