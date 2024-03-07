828153d6a971d35b1a32b2840edb52c1acb93f5d
Trine Rye <trine.rye@hotmail.com>
2024-03-07 16:28:14 +0100

    update

---
+++ assignment_2/logistic_regression.py
@@ -0,0 +1,17 @@
+# %%
+# Logistic Regression Classifier
+import os
+import pandas as pd
+
+# Machine learning stuff
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, ShuffleSplit
+from sklearn import metrics
+
+# Visualisation
+import matplotlib.pyplot as plt
+
+# %%
+
+
